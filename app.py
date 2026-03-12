2883bc9a0b3b09f372ce184eb6b8b26f8b25b92c
Tharun Sathunuru <tharunsathunuri373@gmail.com>
2026-03-12 12:02:47 +0530

    UI fix: cursor issue fixed + improved background overlay

--- app.py
+++ app.py
@@ -7,46 +7,38 @@ load_dotenv()
 app = Flask(__name__)
 
 
-# Home page
+# Home Page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# Weather route
+# Weather Page
 @app.route("/weather")
 def weather():
-    city = request.args.get("city")
+    city = request.args.get("city", "").strip()
 
     if not city:
         return render_template("index.html")
 
     weather_data = get_current_weather(city)
 
-    # City not found
     if not weather_data:
         return render_template("city-not-found.html")
 
-    # Extract data for template
-    title = weather_data["name"]
-    status = weather_data["weather"][0]["description"].title()
-    temp = weather_data["main"]["temp"]
-    feels_like = weather_data["main"]["feels_like"]
-
-    humidity = weather_data["main"]["humidity"]
-    wind_speed = weather_data["wind"]["speed"]
-    icon = weather_data["weather"][0]["icon"]
-
-    return render_template(
-        "weather.html",
-        title=title,
-        status=status,
-        temp=temp,
-        feels_like=feels_like,
-        humidity=humidity,
-        wind_speed=wind_speed,
-        icon=icon
-    )
+    try:
+        return render_template(
+            "weather.html",
+            title=weather_data["name"],
+           status = weather_data["weather"][0]["main"].lower(),
+            temp=weather_data["main"]["temp"],
+            feels_like=weather_data["main"]["feels_like"],
+            humidity=weather_data["main"]["humidity"],
+            wind_speed=weather_data["wind"]["speed"],
+            icon=weather_data["weather"][0]["icon"]
+        )
+    except KeyError:
+        return render_template("city-not-found.html")
 
 
 if __name__ == "__main__":
